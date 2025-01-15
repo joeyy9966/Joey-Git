5009a92b074af5033bc938143c9a93e28dd37229
Nathaniel <nathanieljoey99@gmail.com>
2025-01-15 22:20:04 +0000

    added subFunction, added prodFunction, added divFunction, added powFunction

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
 def addFunction(x, y):
     return x + y
+
+def subFunction(x, y):
+    return x - y
+
+def prodFunction(x, y):
+    return x * y
+
+def divFunction(x, y):
+    return x / y
+
+def powFunction(x, y):
+    return x ** y
